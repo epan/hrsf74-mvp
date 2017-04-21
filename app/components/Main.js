27cc9d041eebfee9e6a67dda6a0c3071201880b7
Eric Pan <epan@users.noreply.github.com>
2017-04-21 16:56:42 -0700

    Render index.html with npm start
    Testing to get React pages to render locally. Still need to make
    it load with node/express locally.
    
    Later will want to also actually make the UI.

---
+++ app/components/Main.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+function Main () {
+  return (
+      <div>Helloww World</div>
+  );
+}
+
+ReactDOM.render(<Main />, document.getElementById('app'));
