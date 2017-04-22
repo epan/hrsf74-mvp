ae720781624ce3a07365bb355396dc310eb03f83
Eric Pan <epan@users.noreply.github.com>
2017-04-21 19:39:50 -0700

    Move bundle.js into ignored folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
   entry: __dirname + '/app/components/Main.jsx',
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/public'
+    path: __dirname + '/public/dist'
   },
   module: {
     loaders: [
