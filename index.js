dae2955dd4e462e8166e11192210920f6b9cf8b9
Eric Pan <epan@users.noreply.github.com>
2017-04-22 11:05:34 -0700

    Use process.env for PORT if available

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
+// TODO: Move this file into /server
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
+const path = require('path');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(logger('tiny'));
 app.use(bodyParser.json());
-app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(path.join(__dirname, '/public')));
 
 app.listen(PORT, () => {
   console.log(`Listening on 127.0.0.1:${PORT}`);
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
